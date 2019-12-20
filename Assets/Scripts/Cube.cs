dc338a6a4a4969c59bf256991861ec2c6018b24e
Geoyeob Kim <gasbank@gmail.com>
2019-12-21 01:38:58 +0900

    큐브 선택, 클릭 이벤트 테스트

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,11 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Cube : MonoBehaviour {
+    [SerializeField] Renderer capRenderer = null;
+
     void OnMouseEnter() {
-        transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+        SetHighlightHeightConditional();
+    }
+
+    void OnMouseOver() {
+        SetHighlightHeightConditional();
     }
 
     void OnMouseExit() {
-        transform.localPosition = new Vector3(transform.localPosition.x, -0.4f, transform.localPosition.z);
+        SetDefaultHeight();
+    }
+
+    void OnMouseDown() {
+        Destroy(gameObject);
+    }
+
+    private void SetHighlightHeightConditional() {
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            SetDefaultHeight();
+            return;
+        }
+        SetHighlightHeight();
+    }
+
+    void SetHighlightHeight() {
+        capRenderer.enabled = true;
+        //transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+    }
+
+    void SetDefaultHeight() {
+        capRenderer.enabled = false;
+        //transform.localPosition = new Vector3(transform.localPosition.x, -0.4f, transform.localPosition.z);
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,39 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour {
-    [SerializeField] Camera mainCam = null;
+//     [SerializeField] Camera mainCam = null;
 
-    Transform currentHit;
+//     Transform currentHit;
 
-    void Start() {
-        UpdateCubeMouseEvent();
-    }
+//     void Start() {
+//         UpdateCubeMouseEvent();
+//     }
 
-    void Update() {
-        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) {
-            UpdateCubeMouseEvent();
-        }
-    }
+//     void Update() {
+//         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0) {
+//             UpdateCubeMouseEvent();
+//         }
+//     }
 
-    void UpdateCubeMouseEvent() {
-        var newHit = QueryCurrentHit();
-        if (currentHit != newHit) {
-            if (currentHit != null) {
-                currentHit.SendMessage("OnMouseExit", SendMessageOptions.DontRequireReceiver);
-            }
-            newHit.SendMessage("OnMouseEnter", SendMessageOptions.DontRequireReceiver);
-            currentHit = newHit;
-        }
-    }
+//     void UpdateCubeMouseEvent() {
+//         if (EventSystem.current.IsPointerOverGameObject()) {
+//             Debug.Log("s");
+//             return;
+//         }
+// Debug.Log("x");
+//         var newHit = QueryCurrentHit();
+//         if (currentHit != newHit) {
+//             if (currentHit != null) {
+//                 currentHit.SendMessage("OnMouseExit", SendMessageOptions.DontRequireReceiver);
+//             }
+//             newHit.SendMessage("OnMouseEnter", SendMessageOptions.DontRequireReceiver);
+//             currentHit = newHit;
+//         }
+//     }
 
-    Transform QueryCurrentHit() {
-        RaycastHit hit;
-        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+//     Transform QueryCurrentHit() {
+//         RaycastHit hit;
+//         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit)) {
-            return hit.transform;
-        }
+//         if (Physics.Raycast(ray, out hit)) {
+//             return hit.transform;
+//         }
 
-        return null;
-    }
+//         return null;
+//     }
 }
