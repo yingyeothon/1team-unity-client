76ebc182d01ca5fd1f76a78e5c64f415b1ae4ba9
Geoyeob Kim <gasbank@gmail.com>
2020-01-29 22:57:24 +0900

    화염 이펙트

---
+++ Assets/Scripts/TimedFlame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TimedFlame : MonoBehaviour
+{
+    public float sec = 10;
+    public GameObject effect;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        sec -= Time.deltaTime;
+        if (sec < 0) {
+            effect.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/TimedFlameSfx.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TimedFlameSfx : MonoBehaviour
+{
+    public float sec1 = 10;
+    public float sec2 = 20;
+    public float sec3 = 30;
+    public AudioSource audioSource;
+    public GameObject sfx;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        sec1 -= Time.deltaTime;
+        sec2 -= Time.deltaTime;
+        sec3 -= Time.deltaTime;
+        if (sec1 < 0) {
+            sfx.SetActive(true);
+            audioSource.volume = 0.3f;
+        }
+        if (sec2 < 0) {
+            audioSource.volume = 0.5f;
+        }
+        if (sec3 < 0) {
+            audioSource.volume = 0.9f;
+        }
+    }
+}
